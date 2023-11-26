b98cf7801f80cc53531fa98cb7f8683d64f6095b
VGlyva <135941893+VGlyva@users.noreply.github.com>
2023-11-26 16:38:54 +0700

    Первый коммит

---
+++ src/main/java/pro/sky/CalcSpringTwo/CalcSpringTwo/CalcSpringTwoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.CalcSpringTwo.CalcSpringTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcSpringTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcSpringTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/CalcSpringTwo/CalcSpringTwo/CalcSpringTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.CalcSpringTwo.CalcSpringTwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcSpringTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
